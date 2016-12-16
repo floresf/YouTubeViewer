41b15514d0e59ec625eacb513cc35696d3474dba
Fernando F <devnando@devnandos-iMac.local>
2016-12-16 14:59:35 -0500

    Initial upload.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCOH9xdmS73p1JMd8cz1PyxLaIl2Qvw2e8';
 
 // Create a new component. This component should produce
@@ -11,10 +13,40 @@ const API_KEY = 'AIzaSyCOH9xdmS73p1JMd8cz1PyxLaIl2Qvw2e8';
 // 	return <div>Hi!</div>;
 // }
 
-const App = () => {
-	return <div>
-		<SearchBar />
-	</div>;
+// ** function version
+// const App = () => {
+// 	return <div>
+// 		<SearchBar />
+// 	</div>;
+// }
+
+// redefing as class so we can use state
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		// using the callback function to add the data to the state
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			// this.setState({videos: videos});
+			// ^ only works when key and name are the same values
+		});
+	}
+
+	render() {
+		return (
+			<div>
+			<SearchBar />
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // Take this component's generated HTML and put it
